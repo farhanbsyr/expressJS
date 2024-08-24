290521a530530966f17507a58d2a8a01932fcdae
unknown <farhanalktr91@gmail.com>
2024-08-24 17:45:26 +0700

    session 2

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,7 @@ import express, { request, response } from "express";
 import routes from "./routes/routes.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import { mockData } from "./utils/constans.mjs";
 // fase pertama setup PORT
 const app = express();
 
@@ -36,4 +37,43 @@ app.get("/", (req, res) => {
   req.session.visited = true;
   res.cookie("hello", "world", { maxAge: 100000, signed: true });
   res.status(200).send({ msg: "Hello World" });
+});
+
+app.post("/api/auth", (request, response) => {
+  const {
+    body: { userName, password },
+  } = request;
+  const findUser = mockData.find((user) => user.userName === userName);
+  if (!findUser || findUser.password !== password)
+    return response.status(401).send({ msg: "BadCredentials" });
+
+  request.session.user = findUser;
+  return response.status(200).send(findUser);
+});
+
+app.get("/api/auth/status", (request, response) => {
+  request.sessionStore.get(request.sessionID, (err, session) => {
+    console.log(session);
+  });
+  return request.session.user
+    ? response.status(200).send(request.session.user)
+    : response.status(401).send({ msg: "Not Authenticated" });
+});
+
+app.post("/api/cart", (request, response) => {
+  if (!request.session.user) return response.sendStatus(401);
+  const { body: item } = request;
+
+  const { cart } = request.session;
+  if (cart) {
+    cart.push(item);
+  } else {
+    request.session.cart = [item];
+  }
+  return response.status(201).send(item);
+});
+
+app.get("/api/cart", (request, response) => {
+  if (!request.session.user) return response.sendStatus(401);
+  return response.send(request.session.cart ?? []);
 });
--- src/utils/constans.mjs
+++ src/utils/constans.mjs
@@ -3,15 +3,18 @@ export const mockData = [
     id: 1,
     userName: "Fadjeri",
     displayName: "Fajdri",
+    password: "hello123",
   },
   {
     id: 2,
     userName: "Farhan",
     displayName: "farhan",
+    password: "hello124",
   },
   {
     id: 3,
     userName: "fadhil",
     displayName: "fadhil",
+    password: "hello125",
   },
 ];
